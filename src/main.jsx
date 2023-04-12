9f3d7db7b0843b262f748a686c964c89b3bc8c13
vickypineli <v.pinero70@gmail.com>
2023-04-12 21:58:14 +0200

    actualizacion de datos

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader,} from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
