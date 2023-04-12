d39864381e85bb64bc83761fe0dbc6dc13aa3c11
vickypineli <v.pinero70@gmail.com>
2023-04-12 18:04:40 +0200

    ruta de contacto

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
