25fe4c06b83d88eef6264997ef7dd098d80e4e69
vickypineli <v.pinero70@gmail.com>
2023-04-12 18:28:49 +0200

    Enrutamiento del lado del cliente

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,5 @@
 
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Root() {
   return (
@@ -32,10 +32,10 @@ export default function Root() {
         <nav>
           <ul>
             <li>
-              <a href={`/contacts/1`}>Your Name</a>
+              <Link to={`contacts/1`}>Your Name</Link>
             </li>
             <li>
-              <a href={`/contacts/2`}>Your Friend</a>
+              <Link to={`contacts/2`}>Your Friend</Link>>
             </li>
           </ul>
         </nav>
