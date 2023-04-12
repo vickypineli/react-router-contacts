0968f05535e7e557fcdbacf126fbf1722d061ae2
vickypineli <v.pinero70@gmail.com>
2023-04-12 17:47:41 +0200

    configuracion enrutado base

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter,  RouterProvider,} from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
