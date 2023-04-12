648c1338257eb92d1a52ef4dffe5d4f2dd85e5d8
vickypineli <v.pinero70@gmail.com>
2023-04-12 23:06:29 +0200

    borrar contacto

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader,} from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact, { action as editAction, } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
