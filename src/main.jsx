f5b83cbab3c853e4c8cd9c7df1949aa514dbe300
vickypineli <v.pinero70@gmail.com>
2023-04-12 21:55:05 +0200

    Parametros de url en cargadores

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {loader as contactLoader,} from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 
 const router = createBrowserRouter([
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
