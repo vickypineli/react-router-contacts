4c674e73622608d71b64e4ce3b05f5d440631ed9
vickypineli <v.pinero70@gmail.com>
2023-04-13 12:55:08 +0200

    reducir la latencia en favoritos con fetcher.state

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -87,6 +87,9 @@ export default function Contact() {
 function Favorite({ contact }) {
   const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
     <fetcher.Form method="post">
       <button
