885c07289e3e2c3fbd3f6b646a3987482216b0d4
vickypineli <v.pinero70@gmail.com>
2023-04-13 08:20:36 +0200

    loading search

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -19,6 +19,12 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+
   useEffect(() => {
     document.getElementById("q").value = q;
   }, [q]);
@@ -32,6 +38,7 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -44,7 +51,7 @@ export default function Root() {
             <div
               id="search-spinner"
               aria-hidden
-              hidden={true}
+              hidden={!searching}
             />
             <div
               className="sr-only"
